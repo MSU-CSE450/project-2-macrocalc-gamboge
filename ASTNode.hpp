77e85f1ca4af89590e1d7903de707da1f5978efe
TheWildBoby <toby@tcw321.com>
2024-09-28 09:24:29 -0400

    AST node setup
    trying to understand these... I think I get them a little more but still have questions outstanding

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -10,15 +10,34 @@
 class ASTNode {
 private:
   // PLACE AST NODE INFO HERE.
+  enum Type {
+    EMPTY = 0,
+    STATEMENT_BLOCK,
+    ASSIGN
+  };
+  Type type{EMPTY};
+  size_t value{0};
+  std::vector<ASTNode> children{};
   
 public:
   // CONSTRUCTORS, ETC HERE.
   // CAN SPECIFY NODE TYPE AND ANY NEEDED VALUES HERE OR USING OTHER FUNCTIONS.
+  ASTNode(Type thisType) {
+    type = thisType;
+  }
 
   // CODE TO ADD CHILDREN AND SETUP AST NODE HERE.
-  void AddChild(ASTNode node) { ; }
+  void AddChild(ASTNode node) {
+    //how do we know which child the node should be? 
+    ;
+  }
   
   // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
-  double Run(SymbolTable & symbols) { ; }
+  double Run(SymbolTable & symbols) { 
+    //recursively call Run on each chilld, if it exists, unless we have the information
+    //on both sides to effectively execute.
+    //how do we know information exists/how do we store that?
+    ; 
+  }
 
 };
