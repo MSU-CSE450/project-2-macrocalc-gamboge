98d00d16710a527a88bd3e06e43522081f9186d5
TheWildBoby <toby@tcw321.com>
2024-09-30 20:24:51 -0400

    ASTNode class work
    should hopefully have the fundamentals to do print("this is a string");

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -12,7 +12,11 @@ public:
   enum Type {
     EMPTY = 0,
     STATEMENT_BLOCK,
-    ASSIGN
+    ASSIGN,
+    PRINT,
+    LEAF_VARIABLE, //variable. To execute, pull this variable from symbol table and return
+    LEAF_STRING, //literal string. eg, print <literal string>
+    LEAF_LITERAL //literal float
   };
 
 private:
@@ -21,6 +25,9 @@ private:
   Type type{EMPTY};
   size_t value{0};  //this is an ID of a variable in the syntax table, if necessary
   std::vector<ASTNode> children{};
+
+  std::string leaf_str_contents; //these two variables used only if the type is leaf_string/leaf_literal
+  double leaf_literal_contents;
   
 public:
   // CONSTRUCTORS, ETC HERE.
@@ -36,11 +43,38 @@ public:
   }
   
   // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
-  double Run(SymbolTable & symbols) { 
-    //recursively call Run on each chilld, if it exists, unless we have the information
+  auto Run(SymbolTable & symbols) { 
+    //recursively call Run on each child, if it exists, unless we have the information
     //on both sides to effectively execute.
     //how do we know information exists/how do we store that?
-    ; 
+    switch (type)
+    {
+
+    case ASSIGN:
+      break;
+    case STATEMENT_BLOCK:
+      break;
+    case PRINT:
+      //TODO: check if printing a statement, or a string
+      std::cout << children[0].Run(symbols);
+      break;
+
+    case LEAF_STRING:
+      //return string contents as string
+      return leaf_str_contents;
+      break;
+    case LEAF_LITERAL:
+      //return literal variable as double
+      //return leaf_literal_contents;
+      break;
+    case LEAF_VARIABLE:
+      //check symbol table for variable.
+      //if not there, ??? (can't just error because it might be a declaration, I think?)
+      break;
+
+    default: //TODO: make enough cases that default is EMPTY, which shouldn't appear
+      break;
+    }
   }
 
 };
