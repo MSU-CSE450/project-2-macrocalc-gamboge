c74b24002d97288d23efd24a43dac25bbb213321
TheWildBoby <toby@tcw321.com>
2024-10-02 13:22:01 -0400

    in class ast changes

--- ASTNode.hpp
+++ ASTNode.hpp
@@ -41,9 +41,13 @@ public:
     //how do we know which child the node should be? 
     ;
   }
+
+  std::vector<ASTNode> GetChildren() {
+
+  }
   
   // CODE TO EXECUTE THIS NODE (AND ITS CHILDREN, AS NEEDED).
-  auto Run(SymbolTable & symbols) { 
+  float Run(SymbolTable & symbols) { 
     //recursively call Run on each child, if it exists, unless we have the information
     //on both sides to effectively execute.
     //how do we know information exists/how do we store that?
@@ -51,25 +55,33 @@ public:
     {
 
     case ASSIGN:
+      //assert(GetChildren.size() == 2);
+      //assert(GetChild(0).GetType() == ASTNode::LEAF_VARIABLE);
+
+      //size_t var_id = GetChild(0).Run();
+      //float assignVal = GetChild(1).Run();
+      //return symbols. VarValue(var_id) = assignVal;
       break;
     case STATEMENT_BLOCK:
       break;
     case PRINT:
-      //TODO: check if printing a statement, or a string
+      //run children; it should return a string (if it's a string) or the result of the calculation
       std::cout << children[0].Run(symbols);
       break;
 
     case LEAF_STRING:
       //return string contents as string
-      return leaf_str_contents;
+      //return leaf_str_contents;
       break;
     case LEAF_LITERAL:
       //return literal variable as double
       //return leaf_literal_contents;
+      //see example code, they just do this.GetWords
       break;
     case LEAF_VARIABLE:
       //check symbol table for variable.
       //if not there, ??? (can't just error because it might be a declaration, I think?)
+      //return symbols.VarValue(GetValue())
       break;
 
     default: //TODO: make enough cases that default is EMPTY, which shouldn't appear
