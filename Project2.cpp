3f8c729c3b5e6882cee181b20666cef24807c31f
SwabhanKatkoori <129548844+SwabhanKatkoori@users.noreply.github.com>
2024-10-04 18:00:05 -0400

    Temp remove AST Node functionality to resolve errors

--- Project2.cpp
+++ Project2.cpp
@@ -5,64 +5,31 @@
 #include <unordered_map>
 #include <vector>
 
-// Below are some suggestions on how you might want to divide up your project.
-// You may delete this and divide it up however you like.
-#include "ASTNode.hpp"
+// // Below are some suggestions on how you might want to divide up your project.
+// // You may delete this and divide it up however you like.
+// #include "ASTNode.hpp"
 #include "lexer.hpp"
 #include "SymbolTable.hpp"
 
 
-//in the example code these were in the header file; I don't think we'll be using that here?
+// //in the example code these were in the header file; I don't think we'll be using that here?
 std::vector<emplex::Token> tokens{};
 size_t token_id{0};
-ASTNode root{ASTNode::STATEMENT_BLOCK};
-SymbolTable symbols{};
-
-
-int main(int argc, char * argv[])
-{
-  if (argc != 2) {
-    std::cout << "Format: " << argv[0] << " [filename]" << std::endl;
-    exit(1);
-  }
-
-  std::string filename = argv[1];
-  
-  std::ifstream in_file(filename);              // Load the input file
-  if (in_file.fail()) {
-    std::cout << "ERROR: Unable to open file '" << filename << "'." << std::endl;
-    exit(1);
-  }
-
-
-  emplex::Lexer lexer;
-  tokens = lexer.Tokenize(in_file); //uuncomment once lexer file exists
-
-  Parse();
-
-  // TO DO:  
-  // PARSE input file to create Abstract Syntax Tree (AST).
-  
-  // Look through each token at a time. Branch to different sections of code based on the token type.
-  // We should never have to look at a previous token ever, if we do it right
-  // Each helper funtion returns an Abstract Syntax Tree Node (I think)
-  // Start with SET and PRINT, as seen in test cases
-  
-  // EXECUTE the AST to run your program.
-  root.Run(symbols);
-}
+// ASTNode root{ASTNode::STATEMENT_BLOCK};
+// SymbolTable symbols{};
 
 
 void Parse() //parse the file. arguments/return probably not final
 {
-  while (token_id < tokens.size()) {
-    //ASTNode cur_node = ParseStatement(); //error? why?
-    //if (cur_node.GetType() != ASTNode::EMPTY) root.AddChild(cur_node);
-  }
+  // while (token_id < tokens.size()) {
+  //   //ASTNode cur_node = ParseStatement(); //error? why?
+  //   //if (cur_node.GetType() != ASTNode::EMPTY) root.AddChild(cur_node);
+  // }
 }
 
+// ASTNode ParseDeclare() {
 
-ASTNode ParseDeclare() {
+int ParseDeclare() {
   /*auto type_token = tokens[token_id++];
   
   std::string var_name{};
@@ -92,5 +59,44 @@ ASTNode ParseDeclare() {
     return out_node;
   }*/
 
-  return ASTNode{ASTNode::EMPTY}; //I think?
+  // return ASTNode{ASTNode::EMPTY}; //I think?
+  return 0;
+}
+
+
+
+int main(int argc, char * argv[])
+{
+  if (argc != 2) {
+    std::cout << "Format: " << argv[0] << " [filename]" << std::endl;
+    exit(1);
+  }
+
+  std::string filename = argv[1];
+  
+  std::ifstream in_file(filename);              // Load the input file
+  if (in_file.fail()) {
+    std::cout << "ERROR: Unable to open file '" << filename << "'." << std::endl;
+    exit(1);
+  }
+
+
+  emplex::Lexer lexer;
+  tokens = lexer.Tokenize(in_file); //uuncomment once lexer file exists
+
+  Parse();
+
+
+  // // TO DO:  
+  // // PARSE input file to create Abstract Syntax Tree (AST).
+  
+  // // Look through each token at a time. Branch to different sections of code based on the token type.
+  // // We should never have to look at a previous token ever, if we do it right
+  // // Each helper funtion returns an Abstract Syntax Tree Node (I think)
+  // // Start with SET and PRINT, as seen in test cases
+  
+  // // EXECUTE the AST to run your program.
+  // root.Run(symbols);
+  std::cout << "hello world" << std::endl;
+  return 0;
 }
